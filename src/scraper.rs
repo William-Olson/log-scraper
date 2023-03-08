11e61dbd552da885e052df5b5c3c251409038994
William Olson <willko747@gmail.com>
2023-03-08 00:31:23 -0600

    fix log-scraper flow + fmt cleanup

--- src/scraper.rs
+++ src/scraper.rs
@@ -1,50 +1,62 @@
 //! # Scraper Module
-//! 
+//!
 //! Attempts to sync and persist logs found on the remote server.
-
-
 
 use crate::{caching, new_relic::NewRelic, storage};
 
 /// Attempts to sync local logs from the remote log service
 pub async fn attempt_sync() {
     let last_seen = match caching::get_cached_val().await {
-      Ok(last_seen) => last_seen.to_string(),
-      Err(err) => {
-          println!("Warning: An error occurred reading from cache: {:?}", err);
-          "".to_owned()
-      }
+        Ok(last_seen) => last_seen.to_string(),
+        Err(err) => {
+            println!("Warning: An error occurred reading from cache: {:?}", err);
+            "".to_owned()
+        }
     };
 
     let nr = NewRelic::new();
     let log_results = nr.logs_since(&last_seen).await;
 
-    if log_results.len() > 0 {
-        nr.print_logs(&log_results);
-
-        let latest_log = nr.find_latest(&log_results);
-        let watermark = nr.to_watermark(&latest_log);
-
-        // Save the logs to disk
-        let filename = storage::get_filename(latest_log.timestamp).await;
-        println!("Writing to file: {} . . .", filename);
-        let data = log_results.iter().map(|l| l.to_string()).collect::<Vec<String>>();
-        match storage::write_to_file(&filename, &data.join("\n")).await {
-          Ok(()) => {
-            println!("Successfully wrote logs to {} √", filename);
-            let line_count = storage::total_lines(&filename).await.unwrap_or(0);
-            println!("Total lines in file: {}", line_count)
-          },
-          Err(err) => println!("Warning: An error occurred saving logs to file: {:?}", err),
-        }
-
-        println!("setting last_seen to timestamp: {}", watermark);
-        match caching::set_cached_val(watermark).await {
-            Ok(()) => println!("Success: saved cached value successfully."),
-            Err(err) => {
-              println!("Warning: An error occurred saving to cache: {:?}", err)
-            },
-        }
+    // bail if there are no new logs to sync
+    if log_results.len() <= 0 {
+        return;
     }
 
+    nr.print_logs(&log_results);
+
+    let latest_log = nr.find_latest(&log_results);
+    let watermark = nr.to_watermark(&latest_log);
+
+    // Save the logs to disk
+    let filename = storage::get_filename(latest_log.timestamp).await;
+    println!("Writing to file: {} . . .", filename);
+    let data = log_results
+        .iter()
+        .map(|l| l.to_string())
+        .collect::<Vec<String>>();
+    
+    let success = match storage::write_to_file(&filename, &data.join("\n")).await {
+        Ok(()) => {
+            println!("Successfully wrote logs to {} √", filename);
+            let line_count = storage::total_lines(&filename).await.unwrap_or(0);
+            println!("Total lines in file: {}", line_count);
+            true
+        }
+        Err(err) => {
+            println!("Warning: An error occurred saving logs to file: {:?}", err);
+            false
+        }
+    };
+
+    if !success {
+        return;
+    }
+
+    println!("setting last_seen to timestamp: {}", watermark);
+    match caching::set_cached_val(watermark).await {
+        Ok(()) => println!("Success: saved cached value successfully."),
+        Err(err) => {
+            println!("Warning: An error occurred saving to cache: {:?}", err)
+        }
+    }
 }
